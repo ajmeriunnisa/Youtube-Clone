868e551bac3cc0dc2156ea54f8a424dfb6b99458
ajmeriunnisa <ajmeriunnisa877@gmail.com>
2025-12-01 20:20:19 +0530

    added Home page with filter bar and responsive video grid

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import useVideos from "../hooks/useVideos"; // custom hook to fetch sample videos
+import VideoCard from "../components/VideoCard";
+import FilterBar from "../components/FilterBar"; // top horizontal category list
+
+const Home = () => {
+  // Getting all videos from our custom hook
+  const videos = useVideos();
+
+  return (
+    <div className="px-4 mt-4">
+
+      {/* ------------------ Horizontal Filter Menu ------------------ */}
+      <FilterBar />
+
+      {/* ------------------ Video Grid Section ------------------ */}
+      <div 
+        className="
+          grid 
+          gap-6 
+          mt-6 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4 
+          xl:grid-cols-5
+        "
+      >
+        {videos.map((video) => (
+          <VideoCard key={video.id} video={video} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
